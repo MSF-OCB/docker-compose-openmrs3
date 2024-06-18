6a5ac473ce8f825818ff85859ebc2cbc983d82e8
Ibrahim Balla <ibrahim.balla@brussels.msf.org>
2024-06-18 11:23:26 +0200

    Update Permissiosn to include tomcat directory to allow deployment of war file

--- deploy/pre-compose-generic.sh
+++ deploy/pre-compose-generic.sh
@@ -24,9 +24,10 @@ docker-compose --verbose --project-directory "${MSFOCB_DEPLOY_DIR}" --ansi never
 
 echo "Starting the openmrs3-emr-${environment} environment..."
 
-
+echo "Changing ownership of /openmrs and /usr/local/tomcat to 1001:0..."
 docker-compose --verbose --project-directory "${MSFOCB_DEPLOY_DIR}" --ansi never --file "${MSFOCB_DEPLOY_DIR}/docker-compose-o3.yml" run -u 0 --rm  backend chown -R 1001:0 /openmrs
 
-#docker-compose --verbose --project-directory "${MSFOCB_DEPLOY_DIR}" --ansi never --file "${MSFOCB_DEPLOY_DIR}/docker-compose-o3.yml" run -u 0 --rm  backend chmod -R 755 /openmrs
+docker-compose --verbose --project-directory "${MSFOCB_DEPLOY_DIR}" --ansi never --file "${MSFOCB_DEPLOY_DIR}/docker-compose-o3.yml" run -u 0 --rm  backend chown -R 1001:0 /usr/local/tomcat
+
 
 echo "Done."
