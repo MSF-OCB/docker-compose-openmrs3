46d4f7623e71f70db6b8f74848af518bcf8fac00
Ibrahim Balla <ibrahim.balla@brussels.msf.org>
2024-06-18 11:57:35 +0200

    Add Tomcat Directory into a volume

--- deploy/pre-compose-generic.sh
+++ deploy/pre-compose-generic.sh
@@ -25,9 +25,9 @@ docker-compose --verbose --project-directory "${MSFOCB_DEPLOY_DIR}" --ansi never
 echo "Starting the openmrs3-emr-${environment} environment..."
 
 echo "Changing ownership of /openmrs and /usr/local/tomcat to 1001:0..."
-docker-compose --verbose --project-directory "${MSFOCB_DEPLOY_DIR}" --ansi never --file "${MSFOCB_DEPLOY_DIR}/docker-compose-o3.yml" run -u 0 --rm  backend chown -R root:root /openmrs
+docker-compose --verbose --project-directory "${MSFOCB_DEPLOY_DIR}" --ansi never --file "${MSFOCB_DEPLOY_DIR}/docker-compose-o3.yml" run -u 0 --rm  backend chown -R 1001:0 /openmrs
 
-docker-compose --verbose --project-directory "${MSFOCB_DEPLOY_DIR}" --ansi never --file "${MSFOCB_DEPLOY_DIR}/docker-compose-o3.yml" run -u 0 --rm  backend chown -R root:root /usr/local/tomcat
+docker-compose --verbose --project-directory "${MSFOCB_DEPLOY_DIR}" --ansi never --file "${MSFOCB_DEPLOY_DIR}/docker-compose-o3.yml" run -u 0 --rm  backend chown -R 1001:0 /usr/local/tomcat
 
 
 echo "Done."
